deb78c65eef3e2a81a6f48fbd11114a4b5c6c59d
Karol Zwolak <karolzwolak7@gmail.com>
2025-10-21 20:38:25 +0200

    hello world on vga

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &str = "Hello, world!";
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.as_bytes().iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x0f; // White on black
+        }
+    }
+
     loop {}
 }
