4b9d13873485b40df3459f0111654b145f173d6b
Karol Zwolak <karolzwolak7@gmail.com>
2025-10-24 19:23:53 +0200

    split into lib and bin

---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+pub mod serial;
+pub mod vga;
+
+use core::panic::PanicInfo;
+
+// Use serial output for panic messages in test mode
+#[allow(clippy::empty_loop)]
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+pub fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    // SAFETY: Writing to the QEMU exit port is safe as long as QEMU is running.
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+/// Entry point for `cargo test`
+#[cfg(test)]
+#[allow(clippy::empty_loop)]
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[test_case]
+fn true_assertion() {
+    let x = 1;
+    assert_eq!(x, 1);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rdos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga;
-
 use core::panic::PanicInfo;
+use rdos::println;
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -16,18 +14,16 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-// Use serial output for panic messages in test mode
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
+    use rdos::test_panic_handler;
+
+    test_panic_handler(info)
 }
 
-#[unsafe(no_mangle)]
 #[allow(clippy::empty_loop)]
+#[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     #[cfg(test)]
     test_main();
@@ -35,51 +31,4 @@ pub extern "C" fn _start() -> ! {
     println!("Hello, World!");
 
     loop {}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    // SAFETY: Writing to the QEMU exit port is safe as long as QEMU is running.
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[test_case]
-fn true_assertion() {
-    let x = 1;
-    assert_eq!(x, 1);
 }
