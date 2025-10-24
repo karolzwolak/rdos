9f118722469702e1c426addf19309bb41fe0d570
Karol Zwolak <karolzwolak7@gmail.com>
2025-10-24 18:32:10 +0200

    add tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga;
 
@@ -14,7 +17,18 @@ fn panic(info: &PanicInfo) -> ! {
 #[unsafe(no_mangle)]
 #[allow(clippy::empty_loop)]
 pub extern "C" fn _start() -> ! {
+    #[cfg(test)]
+    test_main();
+
     println!("Hello, World!");
 
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
