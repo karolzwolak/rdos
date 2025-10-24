38384aefcf2df77e554377db64abb79c4e81fd55
Karol Zwolak <karolzwolak7@gmail.com>
2025-10-24 18:40:54 +0200

    qemu exit codes

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,28 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    // SAFETY: Writing to the QEMU exit port is safe as long as QEMU is running.
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
